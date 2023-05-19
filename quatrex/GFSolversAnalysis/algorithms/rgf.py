b40d6e8fbe582542cdcd4dde9119d8b69418e13c
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2023-05-19 15:28:11 +0200

    Added RGF from the lower left to try the lower computation, without sucess. Commented out

--- quatrex/GFSolversAnalysis/algorithms/rgf.py
+++ quatrex/GFSolversAnalysis/algorithms/rgf.py
@@ -13,33 +13,47 @@ def rgf(A_bloc_diag, A_bloc_upper, A_bloc_lower):
     blockSize = A_bloc_diag.shape[1]
 
     # Storage for the incomplete forward substitution
-    g_diag  = np.zeros((nblocks, blockSize, blockSize))
+    g_diag_left_to_right  = np.zeros((nblocks, blockSize, blockSize))
+    #g_diag_right_to_left  = np.zeros((nblocks, blockSize, blockSize))
 
     # Storage for the full backward substitution 
-    G_diag  = np.zeros((nblocks, blockSize, blockSize))
+    G_diag_left_to_right  = np.zeros((nblocks, blockSize, blockSize))
+    #G_diag_right_to_left  = np.zeros((nblocks, blockSize, blockSize))
+
     G_upper = np.zeros((nblocks-1, blockSize, blockSize))
     G_lower = np.zeros((nblocks-1, blockSize, blockSize))
 
 
     tic = time.perf_counter() # -----------------------------
     # Initialisation of g
-    g_diag[0, ] = np.linalg.inv(A_bloc_diag[0, ])
+    g_diag_left_to_right[0, ] = np.linalg.inv(A_bloc_diag[0, ])
+    #g_diag_right_to_left[-1, ] = np.linalg.inv(A_bloc_diag[-1, ])
 
     # Forward substitution
     for i in range(1, nblocks):
-        g_diag[i, ] = np.linalg.inv(A_bloc_diag[i, ] - A_bloc_lower[i-1, ] @ g_diag[i-1, ] @ A_bloc_upper[i-1, ])
+        g_diag_left_to_right[i, ] = np.linalg.inv(A_bloc_diag[i, ] - A_bloc_lower[i-1, ] @ g_diag_left_to_right[i-1, ] @ A_bloc_upper[i-1, ])
+
+    #for i in range(nblocks-2, -1, -1):
+    #    g_diag_right_to_left[i, ] = np.linalg.inv(A_bloc_diag[i, ] - A_bloc_upper[i, ] @ g_diag_right_to_left[i+1, ] @ A_bloc_lower[i, ])
 
     # Initialisation of last element of G
-    G_diag[-1, ] = g_diag[-1, ]
+    G_diag_left_to_right[-1, ] = g_diag_left_to_right[-1, ]
+    #G_diag_right_to_left[0, ]  = g_diag_right_to_left[0, ]
 
     # Backward substitution
     for i in range(nblocks-2, -1, -1): 
-        G_diag[i, ]  =  g_diag[i, ] @ (np.identity(blockSize) + A_bloc_upper[i, ] @ G_diag[i+1, ] @ A_bloc_lower[i, ] @ g_diag[i, ])
-        G_upper[i, ] = -g_diag[i, ] @ A_bloc_upper[i, ] @ G_diag[i+1, ]
-        G_lower[i, ] = -g_diag[i, ] @ A_bloc_lower[i, ] @ G_diag[i+1, ]
+        G_diag_left_to_right[i, ]  =  g_diag_left_to_right[i, ] @ (np.identity(blockSize) + A_bloc_upper[i, ] @ G_diag_left_to_right[i+1, ] @ A_bloc_lower[i, ] @ g_diag_left_to_right[i, ])
+        G_upper[i, ]               = -g_diag_left_to_right[i, ] @ A_bloc_upper[i, ] @ G_diag_left_to_right[i+1, ]
+        G_lower[i, ]               = -g_diag_left_to_right[i, ] @ A_bloc_lower[i, ] @ G_diag_left_to_right[i+1, ]
+
+    #for i in range(1, nblocks):
+    #    G_diag_right_to_left[i, ]  =  g_diag_right_to_left[i, ] @ (np.identity(blockSize) + A_bloc_lower[i-1, ] @ G_diag_right_to_left[i-1, ] @ A_bloc_upper[i-1, ] @ g_diag_right_to_left[i, ])
+    #    G_lower[i-1, ]             = -g_diag_left_to_right[i-1, ] @ A_bloc_lower[i-1, ] @ G_diag_right_to_left[i-1, ]
     toc = time.perf_counter() # -----------------------------
 
+    #if np.allclose(G_diag_left_to_right, G_diag_right_to_left):
+    #    print("RGF: G_diag_left_to_right and G_diag_right_to_left are close")
 
     print(f"RGF: Inversion took {toc - tic:0.4f} seconds")
-    return G_diag, G_upper, G_lower
+    return G_diag_left_to_right, G_upper, G_lower
 
