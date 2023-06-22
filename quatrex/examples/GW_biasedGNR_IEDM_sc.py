e3790b30afd08a0b759c6ec213ca68fa8d9041bb
dleonard <dleonard@student.ethz.ch>
2023-06-22 11:10:22 +0200

    some analysis with larger energy ranges

--- quatrex/examples/GW_biasedGNR_IEDM_sc.py
+++ quatrex/examples/GW_biasedGNR_IEDM_sc.py
@@ -123,7 +123,7 @@ if __name__ == "__main__":
     # one orbital on C atoms, two same types
     no_orb = np.array([3, 3, 3])
     Vappl = 0.4
-    energy = np.linspace(-18, 22.0, 40001, endpoint = True, dtype = float) # Energy Vector
+    energy = np.linspace(-38, 42.0, 80001, endpoint = True, dtype = float) # Energy Vector
     Idx_e = np.arange(energy.shape[0]) # Energy Index Vector
     hamiltonian_obj = OMENHamClass.Hamiltonian(args.file_hm, no_orb, Vappl = Vappl, rank = rank)
     serial_ham = pickle.dumps(hamiltonian_obj)
@@ -424,7 +424,7 @@ if __name__ == "__main__":
     if rank == 0:
         time_start = -time.perf_counter()
     # output folder
-    folder = '/results/GNR_biased_sc_dacebig/'
+    folder = '/results/GNR_biased_sc_dacehuge/'
     for iter_num in range(max_iter):
 
         comm.Barrier()
