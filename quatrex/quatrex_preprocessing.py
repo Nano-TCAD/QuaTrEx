4a5269fc2fd30f930437b14638433277e7a5b14d
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2023-11-16 16:46:10 +0100

    munor

--- quatrex/quatrex_preprocessing.py
+++ quatrex/quatrex_preprocessing.py
@@ -13,35 +13,39 @@ import toml
 
 
 
-class QuatrexInterface:
+class QuatrexPreprocessing:
     def __init__(
         self,
-        path_to_hamiltonian: str,
-        path_to_overlap_matrix: str,
-        path_to_coulom_matrix: str,
         path_to_parameters_file: str,
-        path_to_atoms_positions: str = None,
-        path_to_position_per_orbitals: str = None
     ):
-        # csr_hamiltonian: sp.csr_matrix = self._read_binnary_matrix(path_to_hamiltonian)	
-        # bsparse_hamiltonian: bsparse = bsparse(csr_hamiltonian)
-        
-        # csr_overlap_matrix: sp.csr_matrix = self._read_binnary_matrix(path_to_overlap_matrix)
-        # bsparse_overlap_matrix: bsparse = bsparse(csr_overlap_matrix)
-        
-        # csr_coulomb_matrix: sp.csr_matrix = self._read_binnary_matrix(path_to_coulom_matrix)
-        # bsparse_coulomb_matrix: bsparse = bsparse(csr_coulomb_matrix)
-        
-        # position_per_orbital: [list, list, list] = self._compute_position_per_orbitals_from_atoms_positions(path_to_atoms_positions, path_to_position_per_orbitals)
-        # Neighboring_matrix_indices: np.ndarray = self._compute_neighboring_matrix(position_per_orbital)
-        
-        #energy_grid, fermi_levels, applied_voltage, conduction_band_edge, temperature, solver_mode = self._read_parameters_from_file(path_to_parameters_file)
         self._simulation_parameters = self._read_parameters_from_file(path_to_parameters_file)
         
-        # energy_array: np.ndarray = self._compute_energy_array(energy_grid)
-        # fermi_levels: [float, float]  = self._compute_fermi_level(fermi_levels, applied_voltage)
+        print(self._simulation_parameters)
         
-        # quatrex = Quatrex_solver(bsparse_hamiltonian, bsparse_overlap_matrix, bsparse_coulomb_matrix, Neighboring_matrix_indices, energy_array, fermi_levels, conduction_band_edge, temperature, solver_mode)
+        # csr_hamiltonian: sp.csr_matrix = self._read_binnary_matrix(self._simulation_parameters.path_to_hamiltonian)	
+        # bsparse_hamiltonian: bsparse = bsparse(csr_hamiltonian)
+        
+        # csr_overlap_matrix: sp.csr_matrix = self._read_binnary_matrix(self._simulation_parameters.path_to_overlap_matrix)
+        # bsparse_overlap_matrix: bsparse = bsparse(csr_overlap_matrix)
+        
+        # csr_coulomb_matrix: sp.csr_matrix = self._read_binnary_matrix(self._simulation_parameters.path_to_coulom_matrix)
+        # bsparse_coulomb_matrix: bsparse = bsparse(csr_coulomb_matrix)
+        
+        # position_per_orbital: [list, list, list] = self._compute_position_per_orbitals_from_atoms_positions(self._simulation_parameters.path_to_atoms_positions, self._simulation_parameters.path_to_position_per_orbitals)
+        # Neighboring_matrix_indices: np.ndarray = self._compute_neighboring_matrix(position_per_orbital)
+        
+        # energy_array: np.ndarray = self._compute_energy_array(self._simulation_parameters.energy_grid)
+        # fermi_levels: [float, float]  = self._compute_fermi_level(self._simulation_parameters.fermi_levels, self._simulation_parameters.applied_voltage)
+        
+        # quatrex = Quatrex_solver(bsparse_hamiltonian, 
+        #                          bsparse_overlap_matrix, 
+        #                          bsparse_coulomb_matrix, 
+        #                          Neighboring_matrix_indices, 
+        #                          energy_array, 
+        #                          fermi_levels, 
+        #                          self._simulation_parameters.conduction_band_edge, 
+        #                          self._simulation_parameters.temperature, 
+        #                          self._simulation_parameters.solver_mode)
 
         
     def solve(
@@ -52,9 +56,10 @@ class QuatrexInterface:
         Returns:
             int: status returned by the solver.
         """
-        status = QuatrexSolver.self_consistency_solve()
+        pass
+        #status = QuatrexSolver.self_consistency_solve()
         
-        return status
+        #return status
     
     
     def get_observables(
--- quatrex/simulation_parameters.py
+++ quatrex/simulation_parameters.py
@@ -4,11 +4,42 @@ from pydantic import BaseModel
 import toml
 
 class SimulationParameters(BaseModel):
+    # Path to the system-matrices
+    #   The system-matrices are given in complex-number float128 binary format.
+    path_to_hamiltonian: str
+    path_to_overlap_matrix: str
+    path_to_coulom_matrix: str
+    
+    # Blocksize of the system-matrices
     blocksize: int
+    
+    # Path to the atoms positions and the position per orbitals matrices.
+    #   The atoms positions and positions per orbitals are given in the xyz format. 
+    #   If atoms positions are given, intern function will deduce the position per
+    #   orbitals from the atoms positions.
+    path_to_atoms_positions: str
+    path_to_position_per_orbitals: str
+    
+    # Conduction band energy [eV]
     conduction_band_energy: float
+    
+    # Temperature in Kelvin [K]
     temperature: float
+    
+    # Solver solver
+    #   - "gf" for Green's Function computation
+    #   - "gw" for electron-electron GW computation
     solver_mode: str
+    
+    # Energy grid
+    #   The energy array will be computed using the left energy range and the
+    #   energy step. Stepping up to the right energy range.
     energy_grid: dict
+    
+    # Fermi levels
+    #  Data validation will be performed to ensure that the given fermi levels
+    #  and applied voltage are consistent with each other. If one is set to 
+    # "None", it will be computed from the other two.
     fermi_levels: dict
     
     
