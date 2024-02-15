7fbd995ad8b9bf9366fa85fdeb3d3fd9dd6553ad
Alexandros Nikolaos Ziogas <alexnick.ziogas@gmail.com>
2024-02-15 16:59:03 +0200

    Updated test script.

--- tests/solvers/rgf_batched_test.py
+++ tests/solvers/rgf_batched_test.py
@@ -5,206 +5,10 @@ import time
 
 from quatrex.block_tri_solvers.rgf_GF_GPU import rgf_standaloneGF_GPU, rgf_standaloneGF_batched_GPU
 from quatrex.block_tri_solvers.rgf_GF_lumi import rgf_standaloneGF_batched_GPU as _rgf_batched
-# from quatrex.block_tri_solvers.rgf_GF_lumi import rgf_standaloneGF_GPU, rgf_standaloneGF_batched_GPU
 
 
 def random_complex(shape, rng: np.random.Generator):
     return rng.random(shape) + 1j * rng.random(shape)
-
-
-# def test_rgf_gpu(num_blocks, num_energies, block_size, repeat=10):
-
-#     rng = np.random.default_rng(42)
-
-#     HD = random_complex((num_blocks, num_energies, block_size, block_size), rng)
-#     HU = random_complex((num_blocks, num_energies, block_size, block_size), rng)
-#     HL = random_complex((num_blocks, num_energies, block_size, block_size), rng)
-#     SGD = random_complex((num_blocks, num_energies, block_size, block_size), rng)
-#     SGU = random_complex((num_blocks, num_energies, block_size, block_size), rng)
-#     SGL = random_complex((num_blocks, num_energies, block_size, block_size), rng)
-#     SLD = random_complex((num_blocks, num_energies, block_size, block_size), rng)
-#     SLU = random_complex((num_blocks, num_energies, block_size, block_size), rng)
-#     SLL = random_complex((num_blocks, num_energies, block_size, block_size), rng)
-
-#     SigGBR = random_complex((num_energies, block_size, block_size), rng)
-#     SigLBR = random_complex((num_energies, block_size, block_size), rng)
-
-#     GL = np.zeros((num_blocks, num_energies, block_size, block_size), dtype=np.complex128)
-#     GLnn1 = np.zeros((num_blocks - 1, num_energies, block_size, block_size), dtype=np.complex128)
-#     GG = np.zeros((num_blocks, num_energies, block_size, block_size), dtype=np.complex128)
-#     GGnn1 = np.zeros((num_blocks - 1, num_energies, block_size, block_size), dtype=np.complex128)
-
-#     DOS = np.zeros((num_energies, num_blocks), dtype=np.complex128)
-#     nE = np.zeros((num_energies, num_blocks), dtype=np.complex128)
-#     nP = np.zeros((num_energies, num_blocks), dtype=np.complex128)
-#     idE = np.zeros((num_energies, num_blocks), dtype=np.complex128)
-
-#     bmin = np.arange(1, num_blocks * block_size - 1, block_size, dtype=np.int32)
-#     bmax = np.arange(block_size, num_blocks * block_size + 1, block_size, dtype=np.int32)
-#     assert len(bmin) == num_blocks
-#     assert len(bmax) == num_blocks
-
-#     HD_d = cp.empty((num_blocks, block_size, block_size), dtype=HD.dtype)
-#     HU_d = cp.empty((num_blocks, block_size, block_size), dtype=HD.dtype)
-#     HL_d = cp.empty((num_blocks, block_size, block_size), dtype=HD.dtype)
-#     SLD_d = cp.empty((num_blocks, block_size, block_size), dtype=HD.dtype)
-#     SLU_d = cp.empty((num_blocks, block_size, block_size), dtype=HD.dtype)
-#     SLL_d = cp.empty((num_blocks, block_size, block_size), dtype=HD.dtype)
-#     SGD_d = cp.empty((num_blocks, block_size, block_size), dtype=HD.dtype)
-#     SGU_d = cp.empty((num_blocks, block_size, block_size), dtype=HD.dtype)
-#     SGL_d = cp.empty((num_blocks, block_size, block_size), dtype=HD.dtype)
-#     SigLBR_d = cp.empty((block_size, block_size), dtype=HD.dtype)
-#     SigGBR_d = cp.empty((block_size, block_size), dtype=HD.dtype)
-
-#     gR_gpu = cp.empty((num_blocks, block_size, block_size), dtype=HD.dtype)
-#     gL_gpu = cp.empty((num_blocks, block_size, block_size), dtype=HD.dtype)
-#     gG_gpu = cp.empty((num_blocks, block_size, block_size), dtype=HD.dtype)
-#     gR_H_gpu = cp.empty((block_size, block_size), dtype=HD.dtype)
-#     gL_tmp = cp.empty((block_size, block_size), dtype=HD.dtype)
-#     gG_tmp = cp.empty((block_size, block_size), dtype=HD.dtype)
-    
-#     runtimes = np.zeros(repeat)
-#     for r in range(repeat):
-
-#         start = time.time()
-
-#         for ie in range(0, num_energies):
-
-#             rgf_standaloneGF_GPU(HD[:, ie],
-#                                  HU[:, ie],
-#                                  HL[:, ie],
-#                                  SGD[:, ie],
-#                                  SGU[:, ie],
-#                                  SGL[:, ie],
-#                                  SLD[:, ie],
-#                                  SLU[:, ie],
-#                                  SLL[:, ie],
-#                                  SigGBR[ie],
-#                                  SigLBR[ie],
-#                                  None, None,  # GR, GRnn1
-#                                  GL[:, ie],
-#                                  GLnn1[:, ie],
-#                                  GG[:, ie],
-#                                  GGnn1[:, ie],
-#                                  DOS[ie],
-#                                  nE[ie],
-#                                  nP[ie],
-#                                  idE[ie],
-#                                  bmin, bmax,
-#                                  HD_d, HU_d, HL_d,
-#                                  SLD_d, SLU_d, SLL_d,
-#                                  SGD_d, SGU_d, SGL_d,
-#                                  SigLBR_d, SigGBR_d,
-#                                  gR_gpu, gL_gpu, gG_gpu,
-#                                  gR_H_gpu, gL_tmp, gG_tmp,
-#                                  None, None,
-#                                  None, None,
-#                                  None, None, None,
-#                                  None, None, None)
-        
-#         runtimes[r] = time.time() - start
-    
-#     print(f"Median non-batched runtime: {np.median(runtimes):.3f} s", flush=True)
-
-#     return
-
-
-# def test_rgf_batched_gpu(num_blocks, num_energies, block_size, batch_size, repeat=10):
-
-#     rng = np.random.default_rng(42)
-
-#     HD = random_complex((num_blocks, num_energies, block_size, block_size), rng)
-#     HU = random_complex((num_blocks, num_energies, block_size, block_size), rng)
-#     HL = random_complex((num_blocks, num_energies, block_size, block_size), rng)
-#     SGD = random_complex((num_blocks, num_energies, block_size, block_size), rng)
-#     SGU = random_complex((num_blocks, num_energies, block_size, block_size), rng)
-#     SGL = random_complex((num_blocks, num_energies, block_size, block_size), rng)
-#     SLD = random_complex((num_blocks, num_energies, block_size, block_size), rng)
-#     SLU = random_complex((num_blocks, num_energies, block_size, block_size), rng)
-#     SLL = random_complex((num_blocks, num_energies, block_size, block_size), rng)
-
-#     SigGBR = random_complex((num_energies, block_size, block_size), rng)
-#     SigLBR = random_complex((num_energies, block_size, block_size), rng)
-
-#     GL = np.zeros((num_blocks, num_energies, block_size, block_size), dtype=np.complex128)
-#     GLnn1 = np.zeros((num_blocks - 1, num_energies, block_size, block_size), dtype=np.complex128)
-#     GG = np.zeros((num_blocks, num_energies, block_size, block_size), dtype=np.complex128)
-#     GGnn1 = np.zeros((num_blocks - 1, num_energies, block_size, block_size), dtype=np.complex128)
-
-#     DOS = np.zeros((num_energies, num_blocks), dtype=np.complex128)
-#     nE = np.zeros((num_energies, num_blocks), dtype=np.complex128)
-#     nP = np.zeros((num_energies, num_blocks), dtype=np.complex128)
-#     idE = np.zeros((num_energies, num_blocks), dtype=np.complex128)
-
-#     bmin = np.arange(1, num_blocks * block_size - 1, block_size, dtype=np.int32)
-#     bmax = np.arange(block_size, num_blocks * block_size + 1, block_size, dtype=np.int32)
-#     assert len(bmin) == num_blocks
-#     assert len(bmax) == num_blocks
-
-#     HD_d = cp.empty((num_blocks, batch_size, block_size, block_size), dtype=HD.dtype)
-#     HU_d = cp.empty((num_blocks, batch_size, block_size, block_size), dtype=HD.dtype)
-#     HL_d = cp.empty((num_blocks, batch_size, block_size, block_size), dtype=HD.dtype)
-#     SLD_d = cp.empty((num_blocks, batch_size, block_size, block_size), dtype=HD.dtype)
-#     SLU_d = cp.empty((num_blocks, batch_size, block_size, block_size), dtype=HD.dtype)
-#     SLL_d = cp.empty((num_blocks, batch_size, block_size, block_size), dtype=HD.dtype)
-#     SGD_d = cp.empty((num_blocks, batch_size, block_size, block_size), dtype=HD.dtype)
-#     SGU_d = cp.empty((num_blocks, batch_size, block_size, block_size), dtype=HD.dtype)
-#     SGL_d = cp.empty((num_blocks, batch_size, block_size, block_size), dtype=HD.dtype)
-#     SigLBR_d = cp.empty((batch_size, block_size, block_size), dtype=HD.dtype)
-#     SigGBR_d = cp.empty((batch_size, block_size, block_size), dtype=HD.dtype)
-
-#     gR_gpu = cp.empty((num_blocks, batch_size, block_size, block_size), dtype=HD.dtype)
-#     gL_gpu = cp.empty((num_blocks, batch_size, block_size, block_size), dtype=HD.dtype)
-#     gG_gpu = cp.empty((num_blocks, batch_size, block_size, block_size), dtype=HD.dtype)
-#     gR_H_gpu = cp.empty((batch_size, block_size, block_size), dtype=HD.dtype)
-#     gL_tmp = cp.empty((batch_size, block_size, block_size), dtype=HD.dtype)
-#     gG_tmp = cp.empty((batch_size, block_size, block_size), dtype=HD.dtype)
-
-#     runtimes = np.zeros(repeat)
-#     for r in range(repeat):
-
-#         start = time.time()
-
-#         for ie in range(0, num_energies, batch_size):
-#             ie_end = min(ie + batch_size, num_energies)
-
-#             rgf_standaloneGF_batched_GPU(HD[:, ie:ie_end],
-#                                          HU[:, ie:ie_end],
-#                                          HL[:, ie:ie_end],
-#                                          SGD[:, ie:ie_end],
-#                                          SGU[:, ie:ie_end],
-#                                          SGL[:, ie:ie_end],
-#                                          SLD[:, ie:ie_end],
-#                                          SLU[:, ie:ie_end],
-#                                          SLL[:, ie:ie_end],
-#                                          SigGBR[ie:ie_end],
-#                                          SigLBR[ie:ie_end],
-#                                          None, None,  # GR, GRnn1
-#                                          GL[:, ie:ie_end],
-#                                          GLnn1[:, ie:ie_end],
-#                                          GG[:, ie:ie_end],
-#                                          GGnn1[:, ie:ie_end],
-#                                          DOS[ie:ie_end],
-#                                          nE[ie:ie_end],
-#                                          nP[ie:ie_end],
-#                                          idE[ie:ie_end],
-#                                          bmin, bmax,
-#                                          HD_d[:, :ie_end - ie], HU_d[:, :ie_end - ie], HL_d[:, :ie_end - ie],
-#                                          SLD_d[:, :ie_end - ie], SLU_d[:, :ie_end - ie], SLL_d[:, :ie_end - ie],
-#                                          SGD_d[:, :ie_end - ie], SGU_d[:, :ie_end - ie], SGL_d[:, :ie_end - ie],
-#                                          SigLBR_d[:ie_end - ie], SigGBR_d[:ie_end - ie],
-#                                          gR_gpu[:, :ie_end - ie], gL_gpu[:, :ie_end - ie], gG_gpu[:, :ie_end - ie],
-#                                          gR_H_gpu[:ie_end - ie], gL_tmp[:ie_end - ie], gG_tmp[:ie_end - ie],
-#                                          None, None,
-#                                          None, None,
-#                                          None, None, None,
-#                                          None, None, None)
-
-#         runtimes[r] = time.time() - start
-    
-#     print(f"Median batched runtime (batch size {batch_size}): {np.median(runtimes):.3f} s", flush=True)
-
-#     return
 
 
 def test_rgf_gpu(num_blocks, num_energies, block_size, repeat=10):
@@ -353,7 +157,44 @@ def test_rgf_batched_gpu(num_blocks, num_energies, block_size, batch_size, repea
 
         runtimes[r] = time.time() - start
     
-    print(f"Median batched runtime (batch size {batch_size}): {np.median(runtimes):.3f} s", flush=True)
+    print(f"Median old batched runtime (batch size {batch_size}): {np.median(runtimes):.3f} s", flush=True)
+
+    runtimes = np.zeros(repeat)
+    istream, ostream = cp.cuda.Stream(non_blocking=True), cp.cuda.Stream(non_blocking=True)
+    for r in range(repeat):
+
+        start = time.time()
+
+        for ie in range(0, num_energies, batch_size):
+            ie_end = min(ie + batch_size, num_energies)
+
+            _rgf_batched(HD[:, ie:ie_end],
+                         HU[:, ie:ie_end],
+                         HL[:, ie:ie_end],
+                         SGD[:, ie:ie_end],
+                         SGU[:, ie:ie_end],
+                         SGL[:, ie:ie_end],
+                         SLD[:, ie:ie_end],
+                         SLU[:, ie:ie_end],
+                         SLL[:, ie:ie_end],
+                         SigGBR[ie:ie_end],
+                         SigLBR[ie:ie_end],
+                         None, None,  # GR, GRnn1
+                         GL[:, ie:ie_end],
+                         GLnn1[:, ie:ie_end],
+                         GG[:, ie:ie_end],
+                         GGnn1[:, ie:ie_end],
+                         DOS[ie:ie_end],
+                         nE[ie:ie_end],
+                         nP[ie:ie_end],
+                         idE[ie:ie_end],
+                         bmin, bmax,
+                         solve=False,
+                         input_stream=istream, output_stream=ostream)
+
+        runtimes[r] = time.time() - start
+    
+    print(f"Median new batched runtime (batch size {batch_size}): {np.median(runtimes):.3f} s", flush=True)
 
     return
 
@@ -440,15 +281,10 @@ def validate_rgf_batched_gpu(num_blocks, batch_size, block_size):
                  GG2, GGnn12,
                  DOS2, nE2, nP2, idE2,
                  bmin, bmax,
-                 solve=True)
+                 solve=False)
     
-    # assert np.allclose(GR, GR2)
-    # assert np.allclose(GL, GL2)
-    # assert np.allclose(GG, GG2)
-    # print(np.linalg.norm(GR - GR2) / np.linalg.norm(GR))
     print(np.linalg.norm(GL - GL2) / np.linalg.norm(GL))
     print(np.linalg.norm(GG - GG2) / np.linalg.norm(GG))
-    # print(np.linalg.norm(GRnn1 - GRnn12) / np.linalg.norm(GRnn1))
     print(np.linalg.norm(GLnn1 - GLnn12) / np.linalg.norm(GLnn1))
     print(np.linalg.norm(GGnn1 - GGnn12) / np.linalg.norm(GGnn1))
     print(np.linalg.norm(DOS - DOS2) / np.linalg.norm(DOS))
@@ -456,21 +292,21 @@ def validate_rgf_batched_gpu(num_blocks, batch_size, block_size):
     print(np.linalg.norm(nP - nP2) / np.linalg.norm(nP))
     print(np.linalg.norm(idE - idE2) / np.linalg.norm(idE))
 
-    print("Validation successful!", flush=True)
-
 
 if __name__ == "__main__":
 
-    # for bsz in (2048, ):
+    for bsz in (416, ):
 
-    #     # test_rgf_gpu(5, 20, bsz, repeat=5)
-    #     # test_rgf_batched_gpu(5, 20, bsz, 1, repeat=5)
-    #     test_rgf_batched_gpu(5, 20, bsz, 2, repeat=5)
-    #     test_rgf_batched_gpu(5, 20, bsz, 5, repeat=5)
-    #     test_rgf_batched_gpu(5, 20, bsz, 10, repeat=5)
-    #     test_rgf_batched_gpu(5, 20, bsz, 20, repeat=5)
+        test_rgf_gpu(13, 100, bsz, repeat=5)
+        test_rgf_batched_gpu(13, 100, bsz, 1, repeat=5)
+        test_rgf_batched_gpu(13, 100, bsz, 2, repeat=5)
+        test_rgf_batched_gpu(13, 100, bsz, 5, repeat=5)
+        test_rgf_batched_gpu(13, 100, bsz, 10, repeat=5)
+        test_rgf_batched_gpu(13, 100, bsz, 20, repeat=5)
+        test_rgf_batched_gpu(13, 100, bsz, 50, repeat=5)
+        test_rgf_batched_gpu(13, 100, bsz, 100, repeat=5)
 
-    for bsz in (128, 256, 512):
-        print(f"Testing with block size {bsz}", flush=True)
+    # for bsz in (128, 256, 512):
+    #     print(f"Testing with block size {bsz}", flush=True)
 
-        validate_rgf_batched_gpu(10, 10, bsz)
+    #     validate_rgf_batched_gpu(10, 10, bsz)
