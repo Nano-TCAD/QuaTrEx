0324b669bd9665428568ade46575105248207914
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2024-02-16 09:38:30 +0100

    Amended test.

--- tests/solvers/rgf_batched_test.py
+++ tests/solvers/rgf_batched_test.py
@@ -120,45 +120,6 @@ def test_rgf_batched_gpu(num_blocks, num_energies, block_size, batch_size, repea
     nP = np.zeros((num_energies, num_blocks), dtype=np.complex128)
     idE = np.zeros((num_energies, num_blocks), dtype=np.complex128)
 
-    bmin = np.arange(1, num_blocks * block_size - 1, block_size, dtype=np.int32)
-    bmax = np.arange(block_size, num_blocks * block_size + 1, block_size, dtype=np.int32)
-    assert len(bmin) == num_blocks
-    assert len(bmax) == num_blocks
-
-    runtimes = np.zeros(repeat)
-    for r in range(repeat):
-
-        start = time.time()
-
-        for ie in range(0, num_energies, batch_size):
-            ie_end = min(ie + batch_size, num_energies)
-
-            rgf_standaloneGF_batched_GPU(HD[:, ie:ie_end],
-                                         HU[:, ie:ie_end],
-                                         HL[:, ie:ie_end],
-                                         SGD[:, ie:ie_end],
-                                         SGU[:, ie:ie_end],
-                                         SGL[:, ie:ie_end],
-                                         SLD[:, ie:ie_end],
-                                         SLU[:, ie:ie_end],
-                                         SLL[:, ie:ie_end],
-                                         SigGBR[ie:ie_end],
-                                         SigLBR[ie:ie_end],
-                                         None, None,  # GR, GRnn1
-                                         GL[:, ie:ie_end],
-                                         GLnn1[:, ie:ie_end],
-                                         GG[:, ie:ie_end],
-                                         GGnn1[:, ie:ie_end],
-                                         DOS[ie:ie_end],
-                                         nE[ie:ie_end],
-                                         nP[ie:ie_end],
-                                         idE[ie:ie_end],
-                                         bmin, bmax)
-
-        runtimes[r] = time.time() - start
-    
-    print(f"Median old batched runtime (batch size {batch_size}): {np.median(runtimes):.3f} s", flush=True)
-
     GL2 = cpx.empty_pinned((num_blocks, num_energies, block_size, block_size), dtype=np.complex128)
     GLnn12 = cpx.empty_pinned((num_blocks - 1, num_energies, block_size, block_size), dtype=np.complex128)
     GG2 = cpx.empty_pinned((num_blocks, num_energies, block_size, block_size), dtype=np.complex128)
@@ -169,51 +130,94 @@ def test_rgf_batched_gpu(num_blocks, num_energies, block_size, batch_size, repea
     nP2 = np.zeros((num_energies, num_blocks), dtype=np.complex128)
     idE2 = np.zeros((num_energies, num_blocks), dtype=np.complex128)
 
-    runtimes = np.zeros(repeat)
-    istream, ostream = cp.cuda.Stream(non_blocking=True), cp.cuda.Stream(non_blocking=True)
-    for r in range(repeat):
+    bmin = np.arange(1, num_blocks * block_size - 1, block_size, dtype=np.int32)
+    bmax = np.arange(block_size, num_blocks * block_size + 1, block_size, dtype=np.int32)
+    assert len(bmin) == num_blocks
+    assert len(bmax) == num_blocks
 
-        start = time.time()
+    for batch_size in (1, 2, 5, 10, 20, 50, 100):
 
-        for ie in range(0, num_energies, batch_size):
-            ie_end = min(ie + batch_size, num_energies)
+        print(f"Running batched RGF with batch size {batch_size}...", flush=True)
 
-            _rgf_batched(HD[:, ie:ie_end],
-                         HU[:, ie:ie_end],
-                         HL[:, ie:ie_end],
-                         SGD[:, ie:ie_end],
-                         SGU[:, ie:ie_end],
-                         SGL[:, ie:ie_end],
-                         SLD[:, ie:ie_end],
-                         SLU[:, ie:ie_end],
-                         SLL[:, ie:ie_end],
-                         SigGBR[ie:ie_end],
-                         SigLBR[ie:ie_end],
-                         None, None,  # GR, GRnn1
-                         GL2[:, ie:ie_end],
-                         GLnn12[:, ie:ie_end],
-                         GG2[:, ie:ie_end],
-                         GGnn12[:, ie:ie_end],
-                         DOS2[ie:ie_end],
-                         nE2[ie:ie_end],
-                         nP2[ie:ie_end],
-                         idE2[ie:ie_end],
-                         bmin, bmax,
-                         solve=True,
-                         input_stream=istream, output_stream=ostream)
+        runtimes = np.zeros(repeat)
+        for r in range(repeat):
 
-        runtimes[r] = time.time() - start
-    
-    print(f"Median new batched runtime (batch size {batch_size}): {np.median(runtimes):.3f} s", flush=True)
+            start = time.time()
 
-    assert np.allclose(GL, GL2)
-    assert np.allclose(GG, GG2)
-    assert np.allclose(GLnn1, GLnn12)
-    assert np.allclose(GGnn1, GGnn12)
-    assert np.allclose(DOS, DOS2)
-    assert np.allclose(nE, nE2)
-    assert np.allclose(nP, nP2)
-    assert np.allclose(idE, idE2)
+            for ie in range(0, num_energies, batch_size):
+                ie_end = min(ie + batch_size, num_energies)
+
+                rgf_standaloneGF_batched_GPU(HD[:, ie:ie_end],
+                                            HU[:, ie:ie_end],
+                                            HL[:, ie:ie_end],
+                                            SGD[:, ie:ie_end],
+                                            SGU[:, ie:ie_end],
+                                            SGL[:, ie:ie_end],
+                                            SLD[:, ie:ie_end],
+                                            SLU[:, ie:ie_end],
+                                            SLL[:, ie:ie_end],
+                                            SigGBR[ie:ie_end],
+                                            SigLBR[ie:ie_end],
+                                            None, None,  # GR, GRnn1
+                                            GL[:, ie:ie_end],
+                                            GLnn1[:, ie:ie_end],
+                                            GG[:, ie:ie_end],
+                                            GGnn1[:, ie:ie_end],
+                                            DOS[ie:ie_end],
+                                            nE[ie:ie_end],
+                                            nP[ie:ie_end],
+                                            idE[ie:ie_end],
+                                            bmin, bmax)
+
+            runtimes[r] = time.time() - start
+        
+        print(f"Median old batched runtime (batch size {batch_size}): {np.median(runtimes):.3f} s", flush=True)
+
+        runtimes = np.zeros(repeat)
+        istream, ostream = cp.cuda.Stream(non_blocking=True), cp.cuda.Stream(non_blocking=True)
+        for r in range(repeat):
+
+            start = time.time()
+
+            for ie in range(0, num_energies, batch_size):
+                ie_end = min(ie + batch_size, num_energies)
+
+                _rgf_batched(HD[:, ie:ie_end],
+                            HU[:, ie:ie_end],
+                            HL[:, ie:ie_end],
+                            SGD[:, ie:ie_end],
+                            SGU[:, ie:ie_end],
+                            SGL[:, ie:ie_end],
+                            SLD[:, ie:ie_end],
+                            SLU[:, ie:ie_end],
+                            SLL[:, ie:ie_end],
+                            SigGBR[ie:ie_end],
+                            SigLBR[ie:ie_end],
+                            None, None,  # GR, GRnn1
+                            GL2[:, ie:ie_end],
+                            GLnn12[:, ie:ie_end],
+                            GG2[:, ie:ie_end],
+                            GGnn12[:, ie:ie_end],
+                            DOS2[ie:ie_end],
+                            nE2[ie:ie_end],
+                            nP2[ie:ie_end],
+                            idE2[ie:ie_end],
+                            bmin, bmax,
+                            solve=True,
+                            input_stream=istream, output_stream=ostream)
+
+            runtimes[r] = time.time() - start
+        
+        print(f"Median new batched runtime (batch size {batch_size}): {np.median(runtimes):.3f} s", flush=True)
+
+        assert np.allclose(GL, GL2)
+        assert np.allclose(GG, GG2)
+        assert np.allclose(GLnn1, GLnn12)
+        assert np.allclose(GGnn1, GGnn12)
+        assert np.allclose(DOS, DOS2)
+        assert np.allclose(nE, nE2)
+        assert np.allclose(nP, nP2)
+        assert np.allclose(idE, idE2)
 
     return
 
@@ -318,11 +322,11 @@ if __name__ == "__main__":
 
         rpt = 1
 
-        test_rgf_gpu(13, 10, bsz, repeat=rpt)
-        test_rgf_batched_gpu(13, 10, bsz, 1, repeat=rpt)
-        test_rgf_batched_gpu(13, 10, bsz, 2, repeat=rpt)
-        test_rgf_batched_gpu(13, 10, bsz, 5, repeat=rpt)
-        test_rgf_batched_gpu(13, 10, bsz, 10, repeat=rpt)
+        # test_rgf_gpu(13, 10, bsz, repeat=rpt)
+        test_rgf_batched_gpu(13, 100, bsz, 1, repeat=rpt)
+        # test_rgf_batched_gpu(13, 10, bsz, 2, repeat=rpt)
+        # test_rgf_batched_gpu(13, 10, bsz, 5, repeat=rpt)
+        # test_rgf_batched_gpu(13, 10, bsz, 10, repeat=rpt)
     #     # test_rgf_batched_gpu(13, 100, bsz, 20, repeat=rpt)
     #     # test_rgf_batched_gpu(13, 100, bsz, 50, repeat=rpt)
     #     # test_rgf_batched_gpu(13, 100, bsz, 100, repeat=rpt)
