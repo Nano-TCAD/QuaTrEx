66a9c0d288f86a1e89d81caf9490a93d5ef54628
Jiang Cao <jiang.cao89@gmail.com>
2024-04-07 09:59:41 +0200

    rgf

--- quatrex/block_sparse/rgf_sparse.py
+++ quatrex/block_sparse/rgf_sparse.py
@@ -43,7 +43,7 @@ def put_block_to_bcsr(v,col_index,ind_ptr,nnz,block_size,
             v[j] = mat[i, col_index[j]]
     return
 
-# compute V@P@V for iblock in a range of off-diagonal and return the dense blocks for several diagonals
+# compute V'@P@V for iblock in a range of off-diagonal and return the dense blocks for several diagonals
 def trimul_bcsr(V,P,col_index,ind_ptr,nnz,block_size,
                         num_blocks,num_diag,dtype,iblock,outndiag1,outndiag2,obc):
     mat = np.zeros((outndiag1+outndiag2+1,block_size,block_size),dtype=dtype)
@@ -67,27 +67,69 @@ def trimul_bcsr(V,P,col_index,ind_ptr,nnz,block_size,
                                     num_blocks,num_diag,iblock,step1)
                 Pblock = get_block_from_bcsr(P,col_index,ind_ptr,nnz,block_size,
                                     num_blocks,num_diag,rowB,step2)
-                tmp = Vblock @ Pblock
+                tmp = Vblock.H @ Pblock
                 for step3 in range(-num_diag,num_diag+1):
                     l=k+step3
                     if ((l>=-outndiag1)and(l<=outndiag2)):
                         Vblock = get_block_from_bcsr(V,col_index,ind_ptr,nnz,block_size,
                                     num_blocks,num_diag,rowC,step3)
-                        mat[l,:,:]=tmp @ Vblock
+                        mat[l,:,:]+=tmp @ Vblock
 
     return mat
 
 # compute M@G for iblock on idiag-th diagonal and return the dense block
 def matmul_bcsr(M,G,col_index,ind_ptr,nnz,block_size,
-                        num_blocks,num_diag,dtype,iblock,idiag):
+                        num_blocks,num_diag,dtype,iblock,outndiag1,outndiag2,obc):
     
-    mat = np.zeros((block_size,block_size),dtype=dtype)
+    mat = np.zeros((outndiag1+outndiag2+1,block_size,block_size),dtype=dtype)
+    # refer iblock as index 0, after 2 jumps will be on diagonal step1+2
+    i=0
+    for step1 in range(-num_diag,num_diag+1):  
+        j=i+step1      
+        rowB=iblock+j
+        for step2 in range(-num_diag,num_diag+1):
+            k=j+step2 
+            rowC=iblock+k 
+            in_range = ((rowB>=0) and (rowB<num_blocks))
+            if (in_range or obc):
+                # case `rowB` be inside the matrix 
+                # or case opposite, but to correct boundary effect
+                if (not in_range): 
+                    rowB = max(0, min(rowB, num_blocks-1))                    
+                Mblock = get_block_from_bcsr(M,col_index,ind_ptr,nnz,block_size,
+                                    num_blocks,num_diag,iblock,step1)
+                Gblock = get_block_from_bcsr(G,col_index,ind_ptr,nnz,block_size,
+                                    num_blocks,num_diag,rowB,step2)
+                if ((k>=-outndiag1)and(k<=outndiag2)):
+                    mat[k,:,:] += Mblock @ Gblock
 
-    Mblock = get_block_from_bcsr(M,col_index,ind_ptr,nnz,block_size,
-                        num_blocks,num_diag,iblock,idiag)
-    Gblock = get_block_from_bcsr(G,col_index,ind_ptr,nnz,block_size,
-                        num_blocks,num_diag,iblock,idiag)
-    
     return mat
+ 
+# forward pass of RGF
+#   solve the left-connected equation system: M@G^r = I and G^< = G^r @ Sig^< @ (G^r)^H
+#   G^> can be implicitly known from the identity: G^r - (G^r)^H = G^> - G^<
+#   several forward passes can be chained together to solve a big system in small partitions,
+#   just need to pass the `sigma_out` and `sigma_out_lesser` into the next function call as
+#   `sigma_in` and `sigma_in_lesser` 
+#
+#   `M` is system matrix, `sigma_scat` and `sigma_scat_lesser` are scattering self-energies
+#   `sigma_in` and `sigma_in_lesser` are boundary incoming self-energies from the first site
+#   `sigma_out` and `sigma_out_lesser` are boundary outgoing self-energies from the last site
+#   `gl` and `gl_lesser` are left-connected Green functions
+#   `start_iblock` and `end_iblock` are starting and ending block index for the partition
+#   `inc` is the incremental direction of transport axis, + for going from 0 to L and - for opposite
+def rgf_forward_pass(M,sigma_scat,sigma_scat_lesser,sigma_in,sigma_in_lesser,
+                     start_iblock,end_iblock,num_blocks,block_size):
+    length = abs(start_iblock - end_iblock)
+    inc = np.sign(end_iblock-start_iblock)
+    gl = np.zeros((length,block_size,block_size),dtype='complex')
+    gl_lesser = np.zeros((length,block_size,block_size),dtype='complex')
+    sigma_out = np.zeros((block_size,block_size),dtype='complex')
+    sigma_out_lesser = np.zeros((block_size,block_size),dtype='complex')
 
+    return gl,gl_lesser,sigma_out,sigma_out_lesser
 
+# backward pass of RGF
+#   solve the fully-connected equation system: M@G^r = I and G^< = G^r @ Sig^< @ (G^r)^H
+def rgf_backward():
+    return
