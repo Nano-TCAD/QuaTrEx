9e200a6a4d73147e36afc83e91c24a223dd8f338
Kosheira1 <dleonard@student.ethz.ch>
2024-07-12 10:23:24 +0200

    removing dependencies for todi

--- setup.py
+++ setup.py
@@ -6,17 +6,7 @@ if __name__ == "__main__":
     setuptools.setup(
             name = "QuaTrEx",
             packages = setuptools.find_packages(),
-            install_requires=["scipy==1.12.0",
-                        "toml",
-                            "cupy==12.3.0",
-                                "mpi4py==3.1.5",
-                                    "dace",
-                                        "argparse",
-                                                "mkl-service",
-                                                    "numba",
-                                                        "sympy",
-                                                        "matplotlib",
-                                                        "h5py"
+            install_requires=[
 
 
             ])
